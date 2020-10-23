df9023e69600c01996ad403318edba956af0f514
Tsurukawa <ryu.yujiro0103@gmail.com>
2020-10-23 11:24:19 +0000

    Add Static Pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class StaicPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get staic_pages_home_url
+    get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
-    get staic_pages_help_url
+    get static_pages_help_url
     assert_response :success
   end
 
