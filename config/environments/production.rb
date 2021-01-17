18ea52a1cef8fccfb1478a8ddd39954e0c7b9f2a
Tsurukawa <ryu.yujiro0103@gmail.com>
2021-01-17 09:08:21 +0000

    Add heroku app URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '.herokuapp.com'
+  host = 'https://stark-tor-86949.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailler::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
