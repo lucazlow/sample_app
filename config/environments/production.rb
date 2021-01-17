803c59229961b0ef0299e3607a040c41ea5d5eef
Tsurukawa <ryu.yujiro0103@gmail.com>
2021-01-17 12:58:49 +0000

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'stark-tor-86949.herokuapp.com'
+  host = 'https://stark-tor-86949.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
